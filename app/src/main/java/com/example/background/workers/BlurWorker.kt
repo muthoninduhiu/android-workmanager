685a73dc0652066d546624f49f90df13f8492211
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2022-11-08 13:00:51 +0300

    update the do work function

--- app/src/main/java/com/example/background/workers/BlurWorker.kt
+++ app/src/main/java/com/example/background/workers/BlurWorker.kt
@@ -2,7 +2,10 @@ package com.example.background.workers
 
 import android.content.ContentValues.TAG
 import android.content.Context
+import android.graphics.BitmapFactory
 import android.graphics.BitmapFactory.decodeResource
+import android.net.Uri
+import android.text.TextUtils
 import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
@@ -15,14 +18,25 @@ class BlurWorker(
 ): Worker(context, workerParam) {
     override fun doWork(): Result {
         val appContext  = applicationContext
-        val resourceId = inputData.getString(KEY_IMAGE_URI)
+        val resourceUri = inputData.getString(KEY_IMAGE_URI)
         makeStatusNotification("Blurring Image", appContext)
 
         return try{
-            val picture = decodeResource(appContext.resources,R.drawable.doggo)
+           // val picture = decodeResource(appContext.resources,R.drawable.doggo)
+            if(TextUtils.isEmpty(resourceUri)){
+                Log.e(TAG,"Invalid URI")
+                throw IllegalArgumentException("Invalid URI")
+            }
+            val resolver  = appContext.contentResolver
+
+            val picture = BitmapFactory.decodeStream(
+                resolver.openInputStream(Uri.parse(resourceUri))
+            )
+
             val output = blurBitmap(picture,appContext)
+
             val outputURI = writeBitmapToFile(appContext,output)
-            makeStatusNotification("Output is: $outputURI", appContext)
+
             
             Result.success()
         }catch (throwable: Throwable){
