89e820f457562cf4d5ad86ed4035d1391176d7c1
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2022-11-08 13:18:27 +0300

    add constructor extend worker class

--- app/src/main/java/com/example/background/workers/CleanUpWorker.kt
+++ app/src/main/java/com/example/background/workers/CleanUpWorker.kt
@@ -1,4 +1,14 @@
 package com.example.background.workers
 
-class CleanUpWorker {
+import android.content.Context
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class CleanUpWorker(
+    context: Context,
+    workerParams: WorkerParameters
+): Worker(context, workerParams) {
+    override fun doWork(): Result {
+        TODO("Not yet implemented")
+    }
 }
